4522b181537de5e81e405710e2775edb80387892
Botir <botirziyatov@gmail.com>
2020-11-11 17:35:56 +0500

    config database

--- src/calendar_planner/settings.py
+++ src/calendar_planner/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = 'calendar_planner.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'calendar_planner',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'root',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
