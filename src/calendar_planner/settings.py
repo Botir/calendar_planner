fc513081bced1b2986d2d4ef5cbcf2feb289a439
Botir <botirziyatov@gmail.com>
2020-11-12 16:55:08 +0500

    template settings

--- src/calendar_planner/settings.py
+++ src/calendar_planner/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR.parent)
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'calendar_planner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.parent.joinpath('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,5 +132,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR.parent.joinpath('static')
+]
 
 STATIC_URL = '/static/'
--- src/event/admin.py
+++ src/event/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Event,Schedule
 
-# Register your models here.
+
+class ScheduleInline(admin.TabularInline):
+    model = Schedule
+    extra = 3
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'start_date' , 'end_date']
+    inlines = [ScheduleInline]
+admin.site.register(Event, EventAdmin)
