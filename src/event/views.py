4bd1de724f9a57c6c6d136f9060347b78f41890a
Botir <botirziyatov@gmail.com>
2020-11-13 17:32:58 +0500

    yangilab olish 13.11.2020q

--- src/event/views.py
+++ src/event/views.py
@@ -1,6 +1,27 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Event
 
 
 def index(request):
-    return render(request,'index.html',{})
+    list_events = Event.objects.order_by('start_date')[:5]
+    list = []
+    for data in list_events:
+        list.append( {
+            'title': data.title,
+            'start': str(data.start_date),
+            'end': str(data.end_date)
+            })
+        schedules = data.schedule_set.all()
+        print(schedules)
+        if schedules:
+            for schedule in schedules:
+                list.append({
+                    'start':f'{schedule.event_date} {schedule.start_time}',
+                    'end':f'{schedule.event_date} {schedule.end_time}',
+                    'title':schedule.title
+
+                    })
+    #print(list)
+    return render(request,'index.html',{"list":json.dumps(list)})
